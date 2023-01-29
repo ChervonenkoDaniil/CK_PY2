0d2e2ac6a3bfa86c067f7b54171488597b186c09
ChervonenkoDaniil <117937133+ChervonenkoDaniil@users.noreply.github.com>
2023-01-29 14:28:09 +0300

    Update lab_2_task2.py

--- lab_2_task2.py
+++ lab_2_task2.py
@@ -14,8 +14,20 @@ BOOKS_DATABASE = [
 
 # TODO написать класс Book
 class Book:
+    """
+    Описывает книги, помещающиеся в библиотеку
+    """
 
     def __init__(self, id_, pages: int, name: str):
+        """
+        Инициализирует параметры класса Book
+        Создает и подготавливает к работе данный класс
+
+        :param id_: Параметр идентификатора книги
+        :param pages: Параметр количества страниц книги
+        :param name: Параметр названия книги
+        """
+        
         if not isinstance(id_, int):
             raise TypeError("This parameter should have int type, check your dict for mistake")
 
@@ -40,40 +52,59 @@ class Book:
 
 # TODO написать класс Library
 class Library:
-    id_list = [book_dict["id"] for book_dict in BOOKS_DATABASE]  # для удобства дальнейшей работы и отсутствия
-                                                                 # повторений, сразу выведем список id книг
-    # При предполагаемом соблюдении порядка сортировки книг в порядке возрастания отказаться от параметра
+    """
+    Описывает библиотеку, куда помещаются книги
+    """
 
-    def __init__(self, books=None):  # инициализируем список книг
+    def __init__(self, books=[]):  # Инициализируем список книг
+        # При инициализации пустого списка PyCharm обозначает это как предупреждение, о том, что значение по умолчанию
+        # изменяемо, поэтому, для устранения предупреждения, в качестве этого значения выбрал None
+        """
+        Инициализирует параметры класса Library
+        Создает и подготавливает к работе данный класс
+
+        :param books: Список книг, располагающихся в библиотеке
+        """
         self.book_list = books
-        if books is None:
-            self.book_list = []
 
-    def get_next_book_id(self) -> int:  # увеличиваем id последней книги на 1
-        return 1 if len(self.book_list) == 0 else self.id_list[-1] + 1
+    def get_next_book_id(self) -> int:  # Увеличиваем id последней книги на 1
+        """
+        Показывает значение идентификатора для следующей книги
+
+        :return: Значение последнего идентификатора, увеличенное на 1
+        """
+
+        return 1 if len(self.book_list) == 0 else [i.book_id for i in self.book_list][-1] + 1
+        
         # При предполагаемом соблюдении порядка сортировки книг в порядке возрастания:
         # return len(self.book_list) + 1
 
     def get_index_by_book_id(self, id_for_search: int) -> int:
+        """
+        Показывает, есть ли книга в наличии, по ее идентификатору
+
+        :param id_for_search: ID книги для осуществления запроса
+        :return: Индекс книги в списке, при ее наличии, или возвращает ValueError
+        """
+        
         if not isinstance(id_for_search, int):
             raise ValueError("This parameter should have int type, check your dict for mistake")
-        if id_for_search not in self.id_list:
-            raise ValueError("Книги с запрашиваемым id не существует")
-        for index, id_ in enumerate(self.id_list):
-            if id_ == id_for_search:
-                return index
+        return [index for index, id_ in enumerate(self.book_list) if id_.book_id == id_for_search][0] if \
+            [index for index, id_ in enumerate(self.book_list) if id_.book_id == id_for_search] is not None\
+            else ValueError("Книги с запрашиваемым id не существует")
+
     # При предполагаемом соблюдении порядка сортировки книг в порядке возрастания:
     # return id_for_search - 1 (вместо enumerate)
 
 
 if __name__ == '__main__':
-    empty_library = Library()  # инициализируем пустую библиотеку
-    print(empty_library.get_next_book_id())  # проверяем следующий id для пустой библиотеки
+    empty_library = Library()  # Инициализируем пустую библиотеку
+    print(empty_library.get_next_book_id())  # Проверяем следующий id для пустой библиотеки
 
     list_books = [
         Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
     ]
-    library_with_books = Library(books=list_books)  # инициализируем библиотеку с книгами
-    print(library_with_books.get_next_book_id())  # проверяем следующий id для непустой библиотеки
+    library_with_books = Library(books=list_books)  # Инициализируем библиотеку с книгами
+    print(library_with_books.get_next_book_id())  # Проверяем следующий id для непустой библиотеки
 
-    print(library_with_books.get_index_by_book_id(1))  # проверяем индекс книги с id = 1
+    print(library_with_books.get_index_by_book_id(1))  # Проверяем индекс книги с id = 1
